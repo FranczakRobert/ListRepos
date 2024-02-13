b71415b9d11093f12b08afb704a53c71ad23539b
RobertFranczak <94225378+RobertFranczak@users.noreply.github.com>
2024-02-13 22:03:50 +0100

    working respinse

--- src/main/java/com/robertfranczak/Task/Model/CompleteResponseData.java
+++ src/main/java/com/robertfranczak/Task/Model/CompleteResponseData.java
@@ -1,5 +1,39 @@
 package com.robertfranczak.Task.Model;
 
-public class CompleteResponseData {
+import org.springframework.http.HttpStatusCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompleteResponseData {
+    private List<RepoResponseData> repoResponseData = new ArrayList<>();
+    private String msg;
+    private HttpStatusCode statusCode;
+
+
+    public CompleteResponseData(List<RepoResponseData> repoResponseData, String msg, HttpStatusCode statusCode) {
+        this.repoResponseData = repoResponseData;
+        this.msg = msg;
+        this.statusCode = statusCode;
+    }
+
+    public List<RepoResponseData> getRepoResponseData() {
+        return repoResponseData;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public HttpStatusCode getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(HttpStatusCode statusCode) {
+        this.statusCode = statusCode;
+    }
 }
--- src/main/java/com/robertfranczak/Task/Services/GitHubApiServiceImpl.java
+++ src/main/java/com/robertfranczak/Task/Services/GitHubApiServiceImpl.java
@@ -1,12 +1,19 @@
 package com.robertfranczak.Task.Services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.robertfranczak.Task.Model.CompleteResponseData;
 import com.robertfranczak.Task.Model.RepoResponseData;
+import com.robertfranczak.Task.POJO.BranchWrapper;
+import com.robertfranczak.Task.POJO.NameWrapper;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class GitHubApiServiceImpl implements GitHubApiService {
@@ -19,10 +26,11 @@ public class GitHubApiServiceImpl implements GitHubApiService {
        headers.add("Accept", "application/json");
    }
 
-    @Override
-    public List<RepoResponseData> getRepositoriesDetails(String username) {
+   @Override
+    public  List<RepoResponseData> getRepositoriesDetails(String username) {
 
         HttpEntity<String> request = new HttpEntity<>("parameters", headers);
+
         RestTemplate rest = new RestTemplate();
 
         ResponseEntity<String> responseEntity = rest.exchange(
@@ -30,21 +38,62 @@ public class GitHubApiServiceImpl implements GitHubApiService {
                 HttpMethod.GET,
                 request,
                 String.class);
+        // Todo  HANDLE THIS
+       CompleteResponseData completeResponseData = new CompleteResponseData(repoResponseData,"DUMMY",responseEntity.getStatusCode());
 
         if (responseEntity.getStatusCode() == HttpStatus.OK)
             fetchBranchesAndSHA(responseEntity);
 
-        return repoResponseData;
+        return completeResponseData.getRepoResponseData();
     }
 
     private void fetchBranchesAndSHA(ResponseEntity<String> responseEntity) {
         String responseBody = responseEntity.getBody();
 
         HttpEntity<String> request = new HttpEntity<>("parameters", headers);
+
         RestTemplate rest = new RestTemplate();
 
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            for (NameWrapper element : objectMapper.readValue(responseBody, new TypeReference<List<NameWrapper>>() {
+            })) {
+
+                if (element.getFork().equals("false"))
+                    responseEntity = rest.exchange(
+                            "https://api.github.com/repos/" + element.getOwnerLogin() + "/" + element.getRepositoryName() + "/branches",
+                            HttpMethod.GET,
+                            request,
+                            String.class);
+
+                String result = responseEntity.getBody();
+                objectMapper = new ObjectMapper();
+
+                try {
+                    if (responseEntity.getStatusCode() == HttpStatus.OK) {
+                        List<BranchWrapper> obj = objectMapper.readValue(result, new TypeReference<List<BranchWrapper>>() {
+                        });
+
+                        createResponse(element, obj);
+                    }
+                } catch (Exception e) {
+                    //IMPLEMENT
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
     }
 
-
+    private void createResponse(NameWrapper element, List<BranchWrapper> obj) {
+        Map<String, String> map = new HashMap<>();
+        for (BranchWrapper branchWrapper : obj) {
+            map.put(branchWrapper.getRepositoryName(), branchWrapper.getCommitWrapper().getSha());
+        }
+        this.repoResponseData.add(new RepoResponseData(element.getRepositoryName(), element.getOwnerLogin(), map));
+    }
 
 }
