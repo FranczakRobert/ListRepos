c83ea9f74a64f46e3185591c3eba08b9b0d3ace5
RobertFranczak <94225378+RobertFranczak@users.noreply.github.com>
2024-02-16 13:12:05 +0100

    remove Javadoc

--- src/main/java/com/robertfranczak/Task/Controller/GitHubAPIController.java
+++ src/main/java/com/robertfranczak/Task/Controller/GitHubAPIController.java
@@ -10,9 +10,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-/**
- * Controller handling GitHub API requests.
- */
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/v1")
